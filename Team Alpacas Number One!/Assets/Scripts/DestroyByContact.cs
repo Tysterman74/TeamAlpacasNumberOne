68f629cdb81c8f475a76c7af299c3a32312264d6
KTZimmerle <kzimmerl@uci.edu>
2015-04-11 14:41:55 -0700

    Added DestroyByContact

---
+++ Team Alpacas Number One!/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+    PlayerState playerstate;
+
+    void Start()
+    {
+        playerstate = GetComponent<PlayerState>();
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.rigidbody.tag == "Player")
+        {
+            Destroy(gameObject);
+            playerstate.loseLife();
+        }
+    }
+}
