7e4418f9a80d36fece20e6d15329d37a0c2e00c2
KTZimmerle <kzimmerl@uci.edu>
2015-04-10 20:18:36 -0700

    Test push

---
+++ Team Alpacas Number One!/Assets/Scripts/Test.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+    public int test = 1;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(test);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
