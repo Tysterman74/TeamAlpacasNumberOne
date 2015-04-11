75cb302508d3f4da2f3bf907257887bf559644f1
Tysterman74 <tynutsathitya@gmail.com>
2015-04-10 21:56:54 -0700

    Added TestPlayer Movement for Line Testing.

---
+++ Team Alpacas Number One!/Assets/Scripts/Portal.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portal : MonoBehaviour {
+
+    public GameObject receivingPortal;
+    public Vector2 size;
+
+    float height;
+    float width;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<BoxCollider2D>().size = size;
+        Camera cam = Camera.main;
+        height = 2.0f * cam.orthographicSize;
+        width = height * cam.aspect;
+        print("Width: " + width);
+        print("Height: " + height);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            //Teleport player to other side.
+            //Assume width for now,
+            Vector3 playerPos = col.transform.position;
+            if (col.GetComponent<Rigidbody2D>().velocity.x > 0)
+            {
+                playerPos.x -= (width - 1);
+            }
+            else
+            {
+                playerPos.x += (width - 1);
+            }
+            col.transform.position = playerPos;
+        }
+    }
+}
---
+++ Team Alpacas Number One!/Assets/Scripts/TylersTestMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TylersTestMovement : MonoBehaviour {
+
+    private float speed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-speed, 0);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, -speed);
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
