723d66ed1ee830d55df0ecb09ee3af4efd3d6de4
Reddeyfish <edrichd@uci.edu>
2015-04-11 10:20:53 -0700

    Looping code

--- Team Alpacas Number One!/Assets/Scripts/LineCollision.cs
+++ Team Alpacas Number One!/Assets/Scripts/LineCollision.cs
@@ -43,44 +43,46 @@ public class LineCollision : MonoBehaviour {
 
         return false;
     }
+
+    public class lineObject
+    {
+        public Vector2 start;
+        public Vector2 end;
+        private GameObject graphic;
+        public lineObject(Vector2 start, Vector2 end, GameObject trailGraphicPrefab)
+        {
+            this.start = start;
+            this.end = end;
+            Vector2 midpoint = (start + end) / 2;
+            Vector2 translation = getTranslation();
+            this.graphic = trailGraphicPrefab;
+            graphic.transform.Translate(midpoint, Space.World);
+            graphic.transform.Rotate(0, 0, 90 - (180 * Mathf.Atan2(translation.x, translation.y) / Mathf.PI), Space.World);
+            graphic.transform.localScale = new Vector3(translation.magnitude * 2, 1, 1);
+
+        }
+
+        public bool intersects(lineObject other)
+        {
+            return (checkDir(this.start, this.end, other.start) != checkDir(this.start, this.end, other.end))
+        && (checkDir(other.start, other.end, this.start) != checkDir(other.start, other.end, this.end));
+        }
+
+        public Vector2 getTranslation()
+        {
+            return end - start;
+        }
+
+        private bool checkDir(Vector2 pt1, Vector2 pt2, Vector2 pt3)
+        {
+            return ((pt2.x - pt1.x) * (pt3.y - pt1.y)) > ((pt3.x - pt1.x) * (pt2.y - pt1.y));
+        }
+
+        public void destroy()
+        {
+            GameObject.Destroy(this.graphic);
+        }
+    }
+
 }
 
-public class lineObject
-{
-    public Vector2 start;
-    public Vector2 end;
-    private GameObject graphic;
-    public lineObject(Vector2 start, Vector2 end, GameObject trailGraphicPrefab)
-    {
-        this.start = start;
-        this.end = end;
-        Vector2 midpoint = ( start + end )/2;
-        Vector2 translation = getTranslation();
-        this.graphic = trailGraphicPrefab;
-        graphic.transform.Translate(midpoint, Space.World);
-        graphic.transform.Rotate(0, 0, 90 - (180 * Mathf.Atan2(translation.x, translation.y) / Mathf.PI), Space.World);
-        graphic.transform.localScale = new Vector3(translation.magnitude * 2, 1, 1);
-
-    }
-
-    public bool intersects(lineObject other)
-    {
-        return (checkDir(this.start,this.end,other.start) != checkDir(this.start,this.end,other.end)) 
-	&& (checkDir(other.start,other.end,this.start) != checkDir(other.start,other.end,this.end));
-    }
-
-    public Vector2 getTranslation()
-    {
-        return end - start;
-    }
-
-    private bool checkDir(Vector2 pt1, Vector2 pt2, Vector2 pt3)
-    {
-        return ((pt2.x-pt1.x)*(pt3.y-pt1.y)) > ((pt3.x-pt1.x)*(pt2.y-pt1.y));
-    }
-
-    public void destroy()
-    {
-        GameObject.Destroy(this.graphic);
-    }
-}
---
+++ Team Alpacas Number One!/Assets/Scripts/PickupBehaviour.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PickupBehaviour : MonoBehaviour {
+
+    public GameObject loopUIElement;
+
+    Dictionary<int, loopObject> currentPositions; //int is player ID, float is their relative position
+
+    // Use this for initialization
+    void Start()
+    {
+        currentPositions = new Dictionary<int, loopObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag != Tags.player)
+            return;
+        currentPositions[1] = new loopObject(other.transform.position - this.transform.position);
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag != Tags.player)
+            return;
+        currentPositions[1].updatePosition(other.transform.position - this.transform.position);
+
+        if (currentPositions[1].loopComplete())
+        {
+            Debug.Log("LOOP COMPLETE!");
+        }
+        
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag != Tags.player)
+            return;
+        currentPositions.Remove(1);
+    }
+
+    public class loopObject
+    {
+        public Vector2 currentPosition;
+        public float totalAngleCovered;
+        public Vector2 startPosition;
+        private GameObject UIArc;
+        public loopObject(Vector2 position)
+        {
+            startPosition = position;
+            currentPosition = position;
+            totalAngleCovered = 0;
+            //UIArc = Instantiate(loopUIElement, this.transform.position, Quaternion.LookRotation());
+        }
+
+        public void updatePosition(Vector2 position)
+        {
+            totalAngleCovered += getAngleBetweenVectors(position, currentPosition);
+            currentPosition = position;
+        }
+
+        public bool loopComplete()
+        {
+            return Mathf.Abs(totalAngleCovered) >= 360;
+        }
+
+        private float getAngleBetweenVectors(Vector2 vectorNew, Vector2 vectorOld)
+        {
+            if (Mathf.Sign(vectorNew.y) < 0 && Mathf.Sign(vectorOld.y) < 0)
+            {
+                if (Mathf.Sign(vectorNew.x) < 0 && Mathf.Sign(vectorOld.x) > 0) //special case; crossing from pi to -pi
+                    return Vector2.Angle(vectorNew, vectorOld);
+                if (Mathf.Sign(vectorNew.x) > 0 && Mathf.Sign(vectorOld.x) < 0) //special case; crossing from -pi to pi
+                    return -Vector2.Angle(vectorNew, vectorOld);
+            }
+
+            return (Mathf.Atan2(vectorNew.x, vectorNew.y) - Mathf.Atan2(vectorOld.x, vectorOld.y)) * Mathf.Rad2Deg;
+        }
+    }
+}
+
