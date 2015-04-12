e125df70371386b0cde2d3606ab3d9a7a6e75b47
Reddeyfish <edrichd@uci.edu>
2015-04-11 22:57:51 -0700

    seal sprite additions

--- Team Alpacas Number One!/Assets/Scripts/SealPowerUp.cs
+++ Team Alpacas Number One!/Assets/Scripts/SealPowerUp.cs
@@ -1,27 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SealPowerUp : PowerUp {
 
+public class SealPowerUp : PowerUp {
+    private GameObject shieldEffect;
+    public GameObject shieldEffectPrefab;
 	public float duration;
 
 	// Use this for initialization
-	void Start () {
-		
-	}
+
 	
 	// Update is called once per frame
 	public override void Update () {
-		print("active");
 		if (activated)
 		{
-			print("active");
 			duration -= Time.deltaTime;
 			if (duration < 0)
 			{
 				//Reset
 				activated = false;
-
+                Destroy(shieldEffect);
 				// change to no longer invincible by enabling player's sphere collider
 				playerHolding.GetComponent<PlayerState>().setInvulnerability(false);
 			}
@@ -34,7 +32,8 @@ public class SealPowerUp : PowerUp {
 
 		//make player invincible
 		playerHolding.GetComponent<PlayerState>().setInvulnerability(true);
-
+        shieldEffect = Instantiate(shieldEffectPrefab, player.transform.position, player.transform.rotation) as GameObject;
+        shieldEffect.transform.parent = player.transform;
 		activated = true;
 		print("Override");
 	}
