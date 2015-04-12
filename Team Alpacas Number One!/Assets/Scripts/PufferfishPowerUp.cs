90264a1c958f0b3356a922cfed242b861f416e2f
Carl Pacheco <clpachec@uci.edu>
2015-04-11 19:09:25 -0700

    adding pufferfish

---
+++ Team Alpacas Number One!/Assets/Scripts/PufferfishPowerUp.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PufferfishPowerUp : PowerUp {
+	PlayerState playerstate;
+
+	// Use this for initialization
+	public override void Start () {
+		playerstate = GetComponent<PlayerState>();
+	}
+	
+	// Update is called once per frame
+	public override void Update () {
+
+	}
+	
+	public override void Activate(GameObject player) {
+
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		playerstate.loseLife();
+		Debug.Log ("Enter");
+	}
+}
