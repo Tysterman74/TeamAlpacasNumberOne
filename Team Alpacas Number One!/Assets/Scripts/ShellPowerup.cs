fff3c53ef49d9f333455c225187e0923dd895a9c
Reddeyfish <edrichd@uci.edu>
2015-04-11 17:48:37 -0700

    minor fixes

---
+++ Team Alpacas Number One!/Assets/Scripts/ShellPowerup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShellPowerup : PowerUp
+{
+    public float speedMultiplier;
+    public float duration;
+    public GameObject shellBulletPrefab;
+
+
+    public override void Activate(GameObject player)
+    {
+        GameObject shell;
+        shell = Instantiate(shellBulletPrefab, player.transform.position, player.transform.rotation) as GameObject;
+        Destroy(shell, duration);
+        shell.GetComponent<Rigidbody>().velocity = speedMultiplier * player.GetComponent<Rigidbody>().velocity;
+        Destroy(this.gameObject);
+    }
+}
