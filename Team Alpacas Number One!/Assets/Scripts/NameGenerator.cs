ab90aacb32d1cfd04c78135e242914cf9861edfa
Tysterman74 <tynutsathitya@gmail.com>
2015-04-12 19:53:16 -0700

    Name Generator and Added GameManager.prefab

---
+++ Team Alpacas Number One!/Assets/Scripts/NameGenerator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NameGenerator : MonoBehaviour {
+
+    public string[] names;
+
+    private List<string> chosenNames;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public string GetRandomName()
+    {
+        return "";
+        //string
+        //while (chosenNames.Contains(
+        //return names[Random.Range(0, names.Length)];
+    }
+}
