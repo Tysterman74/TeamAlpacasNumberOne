c673b3058a9d3c58223dbe8a36c83407125885a9
Tysterman74 <tynutsathitya@gmail.com>
2015-04-12 18:55:59 -0700

    Getting player names to show.

---
+++ Team Alpacas Number One!/Assets/Scripts/PlayerName.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerName : MonoBehaviour {
+
+    public string playerName;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SetName(string pName)
+    {
+        playerName = pName;
+    }
+
+    public string GetName()
+    {
+        return playerName;
+    }
+}
