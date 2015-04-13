f2ea8e57363ce97c82c298978b2b499339ab3ac0
Tysterman74 <tynutsathitya@gmail.com>
2015-04-12 19:57:46 -0700

    Working on Name Generator

--- Team Alpacas Number One!/Assets/Scripts/NameGenerator.cs
+++ Team Alpacas Number One!/Assets/Scripts/NameGenerator.cs
@@ -9,6 +9,7 @@ public class NameGenerator : MonoBehaviour {
     private List<string> chosenNames;
 	// Use this for initialization
 	void Start () {
+        chosenNames = new List<string>();
 	}
 	
 	// Update is called once per frame
@@ -18,7 +19,12 @@ public class NameGenerator : MonoBehaviour {
 
     public string GetRandomName()
     {
-        return "";
+        string name = names[Random.Range(0, names.Length)];
+        while (!chosenNames.Contains(name))
+        {
+            name = names[Random.Range(0, names.Length)];
+        }
+        return name;
         //string
         //while (chosenNames.Contains(
         //return names[Random.Range(0, names.Length)];
